5f77dd5499eada51edcc10ee64f6742b292a3782
paulo <henriquepaulo0606@gmail.com>
2023-07-26 23:35:11 -0300

    Interagindo com onjetos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-const robotron = document.querySelector("#robotron");
 
-robotron.addEventListener("click", dizOI);
 
-function dizOI() {
-  console.log("oi");
+const controle = document.querySelectorAll("[data-controle]")
+
+controle.forEach(elemento => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+  })
+
+});
+
+
+ function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector("[data-contador]") /* Toda vez que clicar no controle ele vai buscar  quem é o controle-contador de cada peça e vai adicionar ou subtrair o poder.*/
+
+  if(operacao === "-") {
+    peca.value = parseInt(peca.value) - 1
+  } else {
+    peca.value = parseInt(peca.value) + 1
+  }
 }
